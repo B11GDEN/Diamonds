99cbd62a971b94111bec4eb233bfef8c47fcf75d
Daniil Mangazeev <mangazeev.di@phystech.edu>
2021-08-24 17:22:25 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .mask import *
-from .tfrecord import make_tfrecord, tfrecord2idx
+# from .tfrecord import make_tfrecord, tfrecord2idx
 
 __all__ = ['get_mask', 'get_labels', 'make_tfrecord', 'tfrecord2idx']
